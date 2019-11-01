a9be9215852988657179b965014581feeef9db1a
preet-singh <preetsingh13@gmail.com>
2019-11-01 14:15:58 -0500

    JS, HTML, CSS updates

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// /* eslint-disable strict */
+
+// $(document).ready(function() {
+//   //enable fullpage.js;
+//   $('#fullpage').fullpage({
+//     anchors: ['home', 'about', 'resume', 'project-section', 'contact'],
+//     scrollOverflow: true,
+//     fitToSection: false,
+//     animateAnchor: false,
+//     bigSectionsDestination: 'top',
+//   });
+// });
